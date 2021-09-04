85aacc4dc0244b10bbaaf87399d5ba29b47c9009
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-09-04 13:28:00 +0200

    Stuff we worked on during the session

--- aitm_data_collection/scraper.py
+++ aitm_data_collection/scraper.py
@@ -1,14 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+from github import Github
+
+
 
 def scraper():
-    from github import Github
-
     # First create a Github instance:
 
     # using an access token
-    g = Github("access_token")
+    g = Github(os.getenv("AITM_TOKEN"))
 
-    g.search_code()
+    result = g.search_repositories(query="language:javascript oidc-client in:file").reversed
+    print(result.totalCount)
+
+    page = result.get_page(1)
+    print(page)
+
+
+    # repositories = g.search_repositories(query='language:python')
+    # for repo in repositories:
+    #     print(repo)
 
 
 if __name__ == '__main__':
+    load_dotenv()
     scraper()
